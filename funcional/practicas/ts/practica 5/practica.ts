8ef752b23a51e76d7f1e7339778bc2387f790433
Martin <arandaschimpf@gmail.com>
2024-11-03 17:22:08 -0300

    Agregada solucion practica 5

--- funcional/practicas/ts/practica 5/practica.ts
+++ funcional/practicas/ts/practica 5/practica.ts
@@ -1,11 +1,12 @@
 // Function type definition
 type Func<T, R> = (arg: T) => R;
 
-// Compose function
 export function compose<T, R extends any>(...fns: Func<any, any>[]): Func<T, R> {
+  return (arg: T) => fns.reduceRight((acc, fn) => fn(acc), arg) as unknown as R;
 }
 
 export function pipe<T, R extends any>(...fns: Func<any, any>[]): Func<T, R> {
+  return (arg: T) => fns.reduce((acc, fn) => fn(acc), arg) as unknown as R;
 }
 
 // Type definitions for our data structure
@@ -47,8 +48,11 @@ const cityLens = fieldLensGenerator<Address>('city');
 export const distribute = <D, Src, R>(...fns: Func<D, any>[]) => (arg: D) => compose<Src, R>(...fns.map(fn => fn(arg)));
 // Composing lenses to focus on the `city` property of a `User`
 export const cityLensForUser: Lens<User, string> = {
-  get: null,
-  set: null
+  get: pipe(addressLens.get, cityLens.get),
+  set: distribute<User, string, User>(
+      addressLens.set,
+      compose<User, (c: string) => Address>(cityLens.set, addressLens.get)
+    )
 };
 
 export const genericLens = <Src extends Record<string, any>, Value = any>(path: string[] | string): Lens<Src, Value> => {
@@ -56,8 +60,30 @@ export const genericLens = <Src extends Record<string, any>, Value = any>(path: 
     path = path.split('.');
   }
 
+  const lenses = path.map(field => fieldLensGenerator(field));
+
   return ({
-    get: null,
-    set: null
+    get: pipe<Src, Value>(...lenses.map(l => l.get)),
+    set: distribute<Src, Value, Src>(
+      ...lenses.map((lens, index) => {
+        const tail = lenses.slice(0, index).map((l, i) => i === 0 ? l.get : l.set)
+        return compose(lens.set, ...tail)
+      })
+    ),
+    // set: (obj: Src) => (value: Value) => {
+    //   return path.reduceRight((acc: any, field, index) => {
+    //     const o = genericLens<Src, any>(path.slice(0, index)).get(obj);
+    //     return fieldLensGenerator(field).set(o)(acc);
+    //   }, value);
+    // },
+    // set: (obj: Src) => (value: Value): Src => {
+    //   const [head, ...tail] = path
+    //   const lens = fieldLensGenerator<Src, any>(head);
+    //   if (tail.length === 0) {
+    //     return lens.set(obj)(value);
+    //   }
+    //   const tailLens = pipe<Src, Func<Value, Src>>(lens.get, genericLens(tail).set)(obj)
+    //   return pipe<Value, Src>(tailLens, lens.set(obj))(value);
+    // }
   })
 }
